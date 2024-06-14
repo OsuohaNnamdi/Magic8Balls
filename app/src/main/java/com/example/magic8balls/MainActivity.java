2c399818e15a0c67043ac52a96d65978f5ab462e
OsuohaNnamdi <nnamdiosuohat@gmail.com>
2024-06-14 10:17:30 -0700

    Magic 8 Balls

---
+++ app/src/main/java/com/example/magic8balls/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.magic8balls;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Random random = new Random();
+
+        int [] array = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        final ImageView picture = findViewById(R.id.imageView2 );
+        final Button button = (Button) findViewById(R.id.button2);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int roll = random.nextInt(5);
+                picture.setImageResource(array[roll]);
+
+            }
+        });
+    }
+}
